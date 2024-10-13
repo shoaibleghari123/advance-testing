1e86608116129ce88af2ca121e391ccf5f5a2674
muhammad_shoaib <muhammad_shoaib@pitb.gov.pk>
2024-10-14 00:21:34 +0500

    -

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,6 +15,7 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::paginate(10);
+
         return view('product', compact('products'));
     }
 
